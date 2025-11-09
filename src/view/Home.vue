98a8df5380bdb120ac20ede778e0dd35c788c3c8
CodeWithJihane <jihaneboulahri8@gmail.com>
2025-11-09 15:41:49 +0100

    restructure folders for project organization

---
+++ src/view/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center mt-10">
+    <h1 class="text-4xl font-bold text-blue-500">Bienvenue sur la page Home</h1>
+    <p class="mt-4 text-gray-400">Ceci est un exemple de page Vue.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
